732e0696af2d13e499306f1fe576407cb39fc231
Dmitriy <zeifd@mail.ru>
2022-04-29 22:23:09 +0700

    Домашнее задание № 36

---
+++ dz_36/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+void Print(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + " ");
+    }
+}
+int Sum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((i + 1) % 2 == 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+int[] mass = new int[4];
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(-100, 100);
+}
+Print(mass);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов на четных позициях: {Sum(mass)}");
