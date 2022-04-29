2728da7289464e5809b0844b582ae5ca9b53ab17
Dmitriy <zeifd@mail.ru>
2022-04-29 22:32:07 +0700

    Домашнее задание № 38

---
+++ dz_38/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 38: Задайте массив вещественных чисел.
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+void Print(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + " ");
+    }
+}
+int Raz(int[] array)
+{
+    int max = array[0];
+    int min = array[1];
+    if (max < min)
+    {
+        min = max;
+        max = array[1];
+    }
+    for (int i = 2; i < array.Length; i++)
+    {
+        if (array[i]>max)
+        {
+            max = array[i];
+        }
+        if (array[i]<min)
+        {
+            min = array[i];
+        }
+    }
+    return (max-min);
+}
+
+int[] mass = new int[5];
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(0, 100);
+}
+Print(mass);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов на четных позициях: {Raz(mass)}");
